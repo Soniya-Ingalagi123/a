9126360dd4039eda67ef156f47e52e0fdd27221b
Soniya-Ingalagi123 <soniyaingalagi18@gmail.com>
2025-06-10 22:28:44 +0530

    save

---
+++ si.java
@@ -0,0 +1,13 @@
+public class si {
+    public static void main(String[] args) {
+        
+        {
+            int p=1000;
+            int q=2000;
+            int r=3000;
+            System.out.println(
+                "si:"+(p+q+r)
+/100            );
+        }
+    }
+}
