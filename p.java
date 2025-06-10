9ab5951a59353429ccbf8c55e98b60702c022b46
Soniya-Ingalagi123 <soniyaingalagi18@gmail.com>
2025-06-10 22:11:55 +0530

    SAve

---
+++ p.java
@@ -0,0 +1,24 @@
+public class p
+{
+    void pos(int n)
+    {
+        if(n>0)
+        {
+            System.out.println("Positive");
+        }
+       /* else if(n<0)
+        {
+            System.out.println("Negative");
+        }
+        else
+        {
+            System.out.println("Zero");
+    }*/}
+
+    public static void main(String[] args)
+    {
+        p obj = new p();
+        obj.pos(5); // Test with a positive number
+         
+    }
+}
